1e127b550ff8a0bdcba49603891d1dd4dbcd6734
gavingui2011 <gavingui2011@gmail.com>
2012-05-22 14:14:22 +0000

    add global message properties in ${message: content assist list, fix issue http://code.google.com/p/tapestrytools/issues/detail?id=27

--- org.apache.tapestrytools.ui/src/org/eclipse/wst/xml/ui/internal/contentassist/tapestry/TapestryContants.java
+++ org.apache.tapestrytools.ui/src/org/eclipse/wst/xml/ui/internal/contentassist/tapestry/TapestryContants.java
@@ -6,4 +6,10 @@ public class TapestryContants {
 	public static String PREFIX_PROP = "prop:";
 	
 	public static String ANNOTATION_PROPERTY = "Property";
+	
+	public static String WEB_XML = "web.xml";
+	public static String WEB_FILTER = "filter";
+	public static String WEB_FILTER_CLASS = "filter-class";
+	public static String TAPESTRY_5_FILTER = "org.apache.tapestry5.TapestryFilter";
+	public static String WEB_FILTER_NAME = "filter-name";
 }
